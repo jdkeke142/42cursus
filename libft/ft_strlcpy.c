445087afd461925dc0f643608f97c138da51851d
Kévin Jimenez <kevin@kevin.localdomain>
2022-10-20 22:44:49 +0200

    Libft progress

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: kjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 14:02:33 by kjimenez          #+#    #+#             */
-/*   Updated: 2022/09/30 14:21:24 by kjimenez         ###   ########.fr       */
+/*   Updated: 2022/10/20 22:43:23 by kjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
